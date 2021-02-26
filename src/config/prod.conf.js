e6adc8ccf69e018ff15c4b6930edf584b5036629
GengHH <18818060415@163.com>
2021-02-26 11:49:43 +0800

    w：修改打包配置

--- src/config/prod.conf.js
+++ src/config/prod.conf.js
@@ -2,7 +2,7 @@
  * @Author: GengHH
  * @Date: 2020-11-25 17:45:12
  * @LastEditors: GengHH
- * @LastEditTime: 2021-01-08 10:00:28
+ * @LastEditTime: 2021-02-26 11:07:41
  * @Description: file content
  * @FilePath: \jb2q-hrm-web\src\config\prod.conf.js
  */
@@ -15,7 +15,9 @@ const config = {
   apiUrl: '//localhost:8081',
   uploadUrl: '//localhost:8085',
   socketUrl: 'https://socket.sndraw.com',
-  defaultImgUrl: '//localhost:8085/static/default.jpg'
+  defaultImgUrl: '//localhost:8085/static/default.jpg',
+  personBasePath: '/pers-api',
+  corpBasePath: '/corp-api'
 };
 
 export default config;
